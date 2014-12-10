20e84ecd4258e2900c32bf399def16ce54ef1fd4
sathlan <chem@sathlan.org>
2014-12-10 00:18:53 -0500

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chem@sathlan.org'
 license          'GPL 3.0'
 description      'Installs ceph on one node with one osd, usefull for testing.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu'
 supports 'centos'
