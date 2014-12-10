7a841a7d6cf21473c28bc6ca67337d58efabb984
sathlan <chem@sathlan.org>
2014-12-10 00:16:21 -0500

    Add missing dev files on rhel/centos.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['ceph_micro']['path'] = '/tmp/micro-ceph'
 if platform_family?("rhel")
   default['ceph_micro']['librados'] = 'librados2'
   default['ceph_micro']['librbd'] = 'librbd1'
+  default['ceph_micro']['ceph-includes'] = 'ceph-devel'
 else
   default['ceph_micro']['librados'] = 'librados-dev'
   default['ceph_micro']['librbd'] = 'librbd-dev'
